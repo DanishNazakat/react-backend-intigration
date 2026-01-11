832377bd9ba5a125257d2bcccacd5076eeed845c
DanishNazakat <danishnazakat636@gmail.com>
2026-01-11 13:12:59 +0500

    react

---
+++ Router/route.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const router=express.Router();
+const {home}=require("../controllers/Auth");
+const{login}=require("../controllers/Auth");
+router.post("/",home);
+router.post("/login",login);
+
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,35 @@
+let express = require('express');
+const cors = require('cors');
+let path = require('path')
+let app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+let PORT=3000;
+const roter=require("./Router/route");
+let mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://danishnazakat:ONiYN5yCLwlGYgDZ@cluster0.rpvrbpd.mongodb.net/?appName=Cluster0",
+    {
+
+})
+
+.then(()=>{
+    console.log("Connected to MongoDB");
+})   
+.catch((err)=>{
+    console.log("Error connectiong to mongoDB",err)
+})
+
+app.use("/api",roter);
+
+
+
+
+app.listen(PORT,()=>{
+    console.log("server is running")
+})
+
+
+
+
